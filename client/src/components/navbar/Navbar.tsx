03f256ae83bec5dba986c5d2be40906d36efed33
Shadl7 <unknown>
2024-12-07 10:34:08 +0100

    Update navbar style

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,15 +1,12 @@
 import React, { ChangeEventHandler, useState } from "react";
-import styles from "./navbar.module.css";
 import { logout } from "@/reducers/userReducer";
 import { getFiles, searchFiles } from "@/actions/file";
 import { showLoader } from "@/reducers/appReducer";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux-ts";
-import { CircleUserRound, Cloud, KeySquare, LogOut, UserRoundPlus } from "lucide-react";
+import { CircleUserRound, Cloud, LogOut } from "lucide-react";
 import DefaultButton from "@/components/ui/button/DefaultButton";
-import AnimatedLink from "@/components/navbar/link/AnimatedLink";
 import AnimatedLogo from "@/components/navbar/logo/AnimatedLogo";
 import Input from "@/components/ui/input/Input";
-
 
 const Navbar = () => {
   const isAuth = useAppSelector(state => state.user.isAuth);
@@ -18,56 +15,60 @@ const Navbar = () => {
   const [searchName, setSearchName] = useState("");
   const [searchTimeout, setSearchTimeout] = useState<number | null>(null);
 
-  const searchChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
+  const searchChangeHandler: ChangeEventHandler<HTMLInputElement> = e => {
     setSearchName(e.target.value);
     if (searchTimeout) {
       clearTimeout(searchTimeout);
     }
     dispatch(showLoader());
     if (e.target.value != "") {
-      setSearchTimeout(setTimeout((value: string) => {
-        dispatch(searchFiles(value));
-      }, 500, e.target.value));
+      setSearchTimeout(
+        setTimeout(
+          (value: string) => {
+            dispatch(searchFiles(value));
+          },
+          500,
+          e.target.value,
+        ),
+      );
     } else {
       dispatch(getFiles(currentDir));
     }
   };
 
   return (
-    <div className={styles.navbar}>
-      <div className={styles.container}>
+    <div className="bg-[#2a2a2a] flex items-center w-full h-[50px] sticky top-0 overflow-hidden">
+      <div
+        className={`min-w-[900px] mx-auto flex w-[60vw] items-center ${!isAuth && "justify-center" /*Center logo*/}`}>
         <AnimatedLogo text={"SECURE CLOUD"} to={"/"}>
           <Cloud color="#c65139" size={32} />
         </AnimatedLogo>
-        {
-          isAuth ? // If user authorized
-            <>
-              <form autoComplete="off"> {/* Use form to break autofill*/}
-                <Input
-                  value={searchName}
-                  onChange={searchChangeHandler}
-                  classnameBox={styles.search}
-                  type="text"
-                  id="search"
-                  autoComplete="off"
-                  placeholder="Search..." />
-              </form>
-              <DefaultButton text="Logout" className={styles.offset_btn}
-                              onClick={() => dispatch(logout())}>
-                <LogOut color="#de6e57" />
-              </DefaultButton>
-              <CircleUserRound className="min-w-10 max-w-10" size={40} color="#c65139" />
-            </>
-            : // If user not authorized
-            <>
-              <AnimatedLink text={"Login"} className={styles.offset_btn} to="/login">
-                <KeySquare color="#de6e57" />
-              </AnimatedLink>
-              <AnimatedLink text={"Registration"} to="/registration">
-                <UserRoundPlus color="#de6e57" />
-              </AnimatedLink>
-            </>
-        }
+        {isAuth && ( // If user authorized
+          <>
+            <form autoComplete="off" className="grow mx-5">
+              {/* Use form to break autofill*/}
+              <Input
+                value={searchName}
+                onChange={searchChangeHandler}
+                type="text"
+                id="search"
+                autoComplete="off"
+                placeholder="Search..."
+              />
+            </form>
+            <DefaultButton
+              text="Logout"
+              className="ml-10 mr-4"
+              onClick={() => dispatch(logout())}>
+              <LogOut color="#de6e57" />
+            </DefaultButton>
+            <CircleUserRound
+              className="min-w-10 max-w-10"
+              size={40}
+              color="#c65139"
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- client/src/components/ui/button/DefaultButton.tsx
+++ client/src/components/ui/button/DefaultButton.tsx
@@ -3,16 +3,20 @@ import { cn } from "@/lib/utils";
 import styled from "styled-components";
 
 type propsType = {
-  children: ReactElement
-  text: string
-  className?: string,
-  classNameBtn?: string,
-  [_: string]: any
-}
+  children: ReactElement;
+  text: string;
+  className?: string;
+  classNameBtn?: string;
+  [_: string]: any;
+};
 
 export const StyledButton = styled.button`
-  background: linear-gradient(90deg, #414141d1 0%, #61616150 53%, #4f4f4f99 100%);
-  padding: 8px 16px;
+  background: linear-gradient(
+    90deg,
+    #414141d1 0%,
+    #61616150 53%,
+    #4f4f4f99 100%
+  );
   margin: 4px;
   font-size: 16px;
   border-radius: 6px;
@@ -20,7 +24,9 @@ export const StyledButton = styled.button`
   color: white; /* White text for contrast */
   cursor: pointer;
   text-transform: uppercase;
-  transition: background-color 0.3s ease, box-shadow 0.3s ease;
+  transition:
+    background-color 0.3s ease,
+    box-shadow 0.3s ease;
   user-select: none;
   display: flex;
   justify-content: center;
@@ -38,11 +44,24 @@ export const StyledButton = styled.button`
   }
 `;
 
-const DefaultButton: FC<propsType> = ({ children, text, className, classNameBtn, ...props }) => {
+const DefaultButton: FC<propsType> = ({
+  children,
+  text,
+  className,
+  classNameBtn,
+  ...props
+}) => {
   return (
     <div className={className}>
-      <StyledButton {...props} className={cn("group/btn", classNameBtn)}>
-        <div className={"mr-1 group-hover/btn:scale-125 transition-all duration-75"}>{children}</div>
+      <StyledButton
+        {...props}
+        className={cn("group/btn py-2 px-4", classNameBtn)}>
+        <div
+          className={
+            "mr-1 group-hover/btn:scale-125 transition-all duration-75"
+          }>
+          {children}
+        </div>
         {text}
       </StyledButton>
     </div>
