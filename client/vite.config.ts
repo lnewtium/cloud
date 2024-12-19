fbe863e9632a655a77db2c6e2d4b977f8a9cd333
Shadl7 <unknown>
2024-12-19 14:32:49 +0100

    Fix favicon. Attempt 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react(), tsconfigPaths()],
+  publicDir: "public",
   server: {
-    port: 3000
+    port: 3000,
   },
   resolve: {
-    alias: [
-      { find: "@", replacement: 'src' }
-    ]
-  }
-})
+    alias: [{ find: "@", replacement: "src" }],
+  },
+});
