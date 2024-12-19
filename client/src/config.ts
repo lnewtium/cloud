74b9a683d5778294bf54819cedb7b1d90bc62ee8
Shadl7 <unknown>
2024-12-19 14:05:32 +0100

    Dev mode auto-switching

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const DEV_MODE = false;
+export const DEV_MODE = !import.meta.env.PROD;
 export const API_URL = DEV_MODE
   ? "http://localhost:5000/api"
   : "https://lnewtium.com/cloud/api/";
